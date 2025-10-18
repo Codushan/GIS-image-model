a73a14dcc3527b8061c9280705c5731ba72878f8
CB <chandrabhushankumar138@gmail.com>
2025-10-19 00:50:57 +0530

    streamlit changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ uploaded_file = st.file_uploader(
 if uploaded_file is not None:
     # Display image
     image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image', use_column_width=True)
+    st.image(image, caption='Uploaded Image', use_container_width =True)
     
     # Create temporary file
     temp_path = "temp_image.jpg"
